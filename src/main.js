da3c5fa10c386f1f92ac7688e33464d1d49bc414
Note-m <ant.myro.shop@gmail.com>
2024-05-12 22:26:30 -0700

    +

--- src/main.js
+++ src/main.js
@@ -4,8 +4,47 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { render } from './js/render-functions';
 import { fetchPhotos } from './js/pixabay-api';
+import simpleLightbox from 'simplelightbox';
 
 const formSearch = document.querySelector('.form-search');
-const btnSearch = document.querySelector('.btn-search');
+const imgGallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
-formSearch.addEventListener('submit', event => {});
+function search(event) {
+  //Скасовую дефолтні дії
+  event.preventDefault();
+  // read searchWords
+  const searchQuery = event.target.elements.searchWords.value.trim();
+  // clean gallery
+  //cheak searchQuery and use izitost for message about error
+  if (searchQuery === '') {
+    return iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+    });
+  }
+  //clean gallery
+  imgGallery.innerHTML = '';
+
+  //use fechphotos for rendering page
+  fetchPhotos(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      imgGallery.innerHTML = render(data.hits);
+      const buider = new simpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionsDelay: 250,
+      });
+      builder.refresh();
+    })
+    .finally(() => {
+      loader.classList.add('display-none');
+    });
+}
+
+formSearch.addEventListener('submit', search);
